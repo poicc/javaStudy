3c7e6d6bc8e094f730374ab6a6feda91b1fd0b79
crq <2631315464@qq.com>
2021-11-10 00:11:56 +0800

    java反射练习

---
+++ src/main/java/com/soft/study/basic/reflection/Person.java
@@ -0,0 +1,30 @@
+package com.soft.study.basic.reflection;
+
+/**
+ * @description:
+ * @author: crq
+ * @create: 2021-11-10 00:06
+ **/
+public class Person {
+    private Integer id;
+    private String name;
+    public static void getPerson() {
+        System.out.println("I am a person");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Person(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Person() {
+    }
+}
